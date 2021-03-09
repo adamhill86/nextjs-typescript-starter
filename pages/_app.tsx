7007048a2cf8aa5242dec54c9913605305438e94
Adam Hill <ahill86@gmail.com>
2021-03-09 17:23:44 -0500

    chore: update CSS files to SCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import '../styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
